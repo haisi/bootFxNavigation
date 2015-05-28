3b9fe5ddb8646da6476efd51dfd80474f39e8712
Hasan Kara <hasan.kara@fhnw.ch>
2015-05-28 13:24:20 +0200

    Init spring boot with JavaFx test project
    Used to test navigation between views.1

---
+++ src/main/java/ch/fhnw/springfx/SpringFxNavigationApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.springfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFxNavigationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFxNavigationApplication.class, args);
+    }
+}
---
+++ src/test/java/ch/fhnw/springfx/SpringFxNavigationApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.fhnw.springfx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringFxNavigationApplication.class)
+public class SpringFxNavigationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
