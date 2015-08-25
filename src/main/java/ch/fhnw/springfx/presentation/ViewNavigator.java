23546172306cbe37c6dcaf8f6669d5cd5fc98920
Hasan Kara <hasan.kara@fhnw.ch>
2015-08-25 13:33:51 +0200

    Add viewHistory to navigator and instead of getting the view passed, the navigator creates the bean

--- src/main/java/ch/fhnw/springfx/presentation/ViewNavigator.java
+++ src/main/java/ch/fhnw/springfx/presentation/ViewNavigator.java
@@ -3,15 +3,24 @@ package ch.fhnw.springfx.presentation;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.Stack;
 
 /**
  * @author Hasan Kara <hasan.kara@fhnw.ch>
  */
 @Service
 public class ViewNavigator {
+
+    @Inject
+    ApplicationContext appContext;
+
+    private final Stack<Class<? extends AbstractFxmlView>> viewHistory = new Stack<>();
 
     private final ObjectProperty<Node> mainView = new SimpleObjectProperty(this, "mainView", null);
 
@@ -23,7 +32,24 @@ public class ViewNavigator {
         return mainView.get();
     }
 
-    public final void setMainView(Node mainView) {
-        this.mainView.set(mainView);
+    public final void setMainView(Class<? extends AbstractFxmlView> viewClass) {
+        Parent newMainView = createNewBeanInstanceOfView(viewClass);
+        this.mainView.set(newMainView);
+
+        viewHistory.add(viewClass);
+    }
+
+    public final void stepBack() {
+        if (viewHistory.size() > 1) {
+            viewHistory.pop();
+            this.mainView.set(createNewBeanInstanceOfView(viewHistory.peek()));
+
+        } else {
+            System.out.println("Can't go further back");
+        }
+    }
+
+    private Parent createNewBeanInstanceOfView(Class<? extends AbstractFxmlView> viewClass) {
+        return (appContext.getBean(viewClass)).getView();
     }
 }
